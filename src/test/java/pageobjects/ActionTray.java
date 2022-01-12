6e12379078762dc82cae8c59857f338bfb99a99b
stanislav741 <stasdmitruk1@gmail.com>
2022-01-12 04:15:15 +0100

    Finished the test for TC#1.1

--- src/test/java/pageobjects/ActionTray.java
+++ src/test/java/pageobjects/ActionTray.java
@@ -1,8 +1,14 @@
 package pageobjects;
 
+import org.junit.jupiter.api.DisplayName;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
 public class ActionTray {
 
+    @DisplayName("Click the purchase button on the bottom Action Tray")
     public static void actionTrayAddToCart() {
-
+        $(byXpath("//div[@data-autom='addToCart']")).click();
     }
 }
--- src/test/java/tests/AddToCartTests.java
+++ src/test/java/tests/AddToCartTests.java
@@ -8,29 +8,33 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class AddToCartTests {
 
     @Test
     void addToCartMPB16() {
         Homepage.homepageOpen();
+
         Navbar.navbarSelectMac();
         $(".chapternav-item-macbook-pro").shouldBe(visible);
+
         ChapterNav.chapterNavSelectMBP();
         $(".chapternav-item-macbook-pro-14-and-16").shouldBe(visible);
+
         ChapterNav.chapterNavSelectMBP1416();
         $(byText("14\" und 16\" Modelle")).shouldBe(visible);
+
         LocalNav.localNavPurchaseMBP1416();
         $(byText("Wähle dein neues MacBook Pro.")).shouldBe(visible);
+
         BundleSelection.bundleSelectMBP16();
         $(byXpath("//div[@data-autom='mac-product-summary-16inch-better']")).shouldHave(text("512 GB SSD Speicher"));
         $(byXpath("//div[@data-autom='mac-product-summary-16inch-better']")).shouldHave(text("16\" Liquid Retina XDR Display"));
+
         $(byXpath("//button[@data-autom='proceed-16inch-better']")).click();
-        $(byXpath("//div[@data-autom='addToCart']")).click();
-        $(byXpath("//div[@data-autom='add-to-cart']")).click();
 
+        ActionTray.actionTrayAddToCart();
+        $(byXpath("//span[@data-autom='summaryHeaderTitle']")).shouldHave(text("16\" MacBook Pro – Space Grau"));
     }
-
 
 }
